43d3e66ec71d39fcaa85777e42dec7cc07706a90
riccardo frosini <riccardo@reflection.io>
2023-10-22 19:27:57 +0100

    stuff.

--- src/main/java/ai/maths/sat3/probability/SplitClauses.java
+++ src/main/java/ai/maths/sat3/probability/SplitClauses.java
@@ -2,6 +2,7 @@ package ai.maths.sat3.probability;
 
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import ai.maths.sat3.model.CNF;
 import ai.maths.sat3.model.ClauseBuilder;
@@ -12,40 +13,55 @@ public class SplitClauses {
 
     private final DisjunctOfSingletonsOrSingleton first;
     private final Set<DisjunctOfSingletonsOrSingleton> rest;
-    private final Set<DisjunctOfSingletonsOrSingleton> disconnectedFromFirst;
+    private final Set<DisjunctOfSingletonsOrSingleton> negatedClauses;
+    private final Set<DisjunctOfSingletonsOrSingleton> positiveClauses;
+    private final Set<DisjunctOfSingletonsOrSingleton> independentClauses;
 
     private SplitClauses(DisjunctOfSingletonsOrSingleton first, Set<DisjunctOfSingletonsOrSingleton> rest) {
         this.first = first;
         this.rest = rest;
-        this.disconnectedFromFirst = makeRestIndependentFromFirst();
+        this.negatedClauses = buildNegatedClauses();
+        this.positiveClauses = buildPositiveClauses();
+        this.independentClauses = buildIndependentClauses();
     }
 
     public static SplitClauses split(CNF<?> conjuncts) {
-        DisjunctOfSingletonsOrSingleton anySubClause = conjuncts.getAnySubClause(); //TODO choose best sub-clause
+        DisjunctOfSingletonsOrSingleton anySubClause = conjuncts.getAnySubClause();
         Set<DisjunctOfSingletonsOrSingleton> rest = conjuncts.getSubClauses().filter(t -> t != anySubClause).collect(Collectors.toUnmodifiableSet());
         return new SplitClauses(anySubClause, rest);
     }
 
-    private Set<DisjunctOfSingletonsOrSingleton> makeRestIndependentFromFirst() {
-        Set<DisjunctOfSingletonsOrSingleton> disjunctsWithoutNegativeVars = rest.stream()
+    private Set<DisjunctOfSingletonsOrSingleton> buildNegatedClauses() {
+        return rest.stream()
                 .filter(disjunct ->
                         disjunct.getSingletons().stream()
-                                .noneMatch(singleton -> first.getSingletons().contains((Singleton) ClauseBuilder.buildNegation(singleton))))
-                .collect(Collectors.toSet());
-        Set<DisjunctOfSingletonsOrSingleton> toReplace = disjunctsWithoutNegativeVars.stream()
+                                .anyMatch(singleton -> first.getSingletons().contains((Singleton) ClauseBuilder.buildNegation(singleton))))
+                .collect(Collectors.toUnmodifiableSet());
+    }
+
+    private Set<DisjunctOfSingletonsOrSingleton> buildPositiveClauses() {
+        return rest.stream()
                 .filter(disjunct -> disjunct.getSingletons().stream()
-                        .anyMatch(singleton -> first.getSingletons().contains(singleton)))
-                .collect(Collectors.toSet());
-        disjunctsWithoutNegativeVars.removeAll(toReplace);
-        Set<DisjunctOfSingletonsOrSingleton> newClauses = toReplace.stream()
+                        .anyMatch(singleton -> first.getSingletons().contains(singleton)) &&
+                        !negatedClauses.contains(disjunct))
                 .map(disjunct -> disjunct.getSubClauses()
                         .filter(singleton -> !first.getSingletons().contains(singleton))
                         .collect(Collectors.toSet()))
                 .filter(singletons -> singletons.size() != 0)
                 .map(ClauseBuilder::buildDisjunctsOfSingletons)
-                .collect(Collectors.toSet());
-        disjunctsWithoutNegativeVars.addAll(newClauses);
-        return disjunctsWithoutNegativeVars;
+                .collect(Collectors.toUnmodifiableSet());
+    }
+
+    private Set<DisjunctOfSingletonsOrSingleton> buildIndependentClauses() {
+        return rest.stream()
+                .filter(disjunct -> disjunct.getSingletons().stream()
+                        .noneMatch(singleton -> first.getSingletons().contains(singleton)) &&
+                        !negatedClauses.contains(disjunct))
+                .collect(Collectors.toUnmodifiableSet());
+    }
+
+    public Set<DisjunctOfSingletonsOrSingleton> getDisconnectedFromFirst() {
+        return Stream.concat(positiveClauses.stream(), independentClauses.stream()).collect(Collectors.toUnmodifiableSet());
     }
 
     public DisjunctOfSingletonsOrSingleton getFirst() {
@@ -56,7 +72,15 @@ public class SplitClauses {
         return rest;
     }
 
-    public Set<DisjunctOfSingletonsOrSingleton> getDisconnectedFromFirst() {
-        return disconnectedFromFirst;
+    public Set<DisjunctOfSingletonsOrSingleton> getNegatedClauses() {
+        return negatedClauses;
+    }
+
+    public Set<DisjunctOfSingletonsOrSingleton> getPositiveClauses() {
+        return positiveClauses;
+    }
+
+    public Set<DisjunctOfSingletonsOrSingleton> getIndependentClauses() {
+        return independentClauses;
     }
 }
